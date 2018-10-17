cfca37f0808fa642ce2da59ed097973ea70d9b10
peter <peter@vm.com>
2018-10-17 18:20:11 +0800

    use py requests

--- ptt_crawler.py
+++ ptt_crawler.py
@@ -1,45 +1,20 @@
 import os,time
 from datetime import datetime
 from bs4 import BeautifulSoup
+import json, requests
 
 ptt_url = 'https://www.ptt.cc/bbs/index.html'
-board_url = 'https://www.ptt.cc/bbs/{board}/index{index}.html'
 
-html = '{name}_{time}.html'
-
-def parse_board(html, board):
-    soup = BeautifulSoup(html, 'html.parser')
-    articles = list()
-    for link in soup.find_all('a'):
-        if board in link.get('href'):
-            print link.get('href'), link.string
 
 def parse_ptt(html):
     soup = BeautifulSoup(html, 'html.parser')
     boards = list()
     for div in soup.find_all('div', class_={'board-name'}):
-        print div
         boards.append(div.string)
     return boards
 
-ptt_index_file = html.format(name='index', time=datetime.now().strftime("%Y%m%d_%H%M%S"))
-os.system("wget "+ptt_url+" -O "+ptt_index_file)
-
-with open(ptt_index_file, 'r') as index_file:
-    ptt_index = index_file.read()
-
-boards = parse_ptt(ptt_index)
-for b in boards[:2]:
-    print 'download index '+b
-    board_index_file = html.format(name=b, time=datetime.now().strftime("%Y%m%d_%H%M%S"))
-    os.system(
-        "wget " +
-        board_url.format(board=b, index='') +
-        " -O " +
-        board_index_file
-    )
-    with open(board_index_file, 'r') as index_file:
-        board_index = index_file.read()
-    
-    parse_board(board_index, b)
-    time.sleep(5)
+r = requests.get(ptt_url)
+if r.status_code == 200:
+    boards = parse_ptt(r.text)
+    with open('boards.json', 'w') as outfile:
+        json.dump(boards, outfile)
